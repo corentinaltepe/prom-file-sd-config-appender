2a584de1a504afafad81aead3564422e3d8640ce
Corentin <corentin.altepe@gmail.com>
2020-09-12 08:43:32 +0200

    Added commandline parser with Add verb.

--- PromFileSdConfigAppender/Program.cs
+++ PromFileSdConfigAppender/Program.cs
@@ -1,18 +1,37 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using CommandLine;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace PromFileSdConfigAppender
 {
+    [Verb("add", HelpText = "Add a target and job to the configuration file.")]
+    class AddOptions
+    {
+        [Option('j', "job", Required = true, HelpText = "Unique name of the job to which the target belongs.")]
+        public string Job { get; set; }
+
+        [Option('t', "target", Required = true, HelpText = "Target to be added.")]
+        public string Target { get; set; }
+
+        [Option('c', "config-path", Default = "targets.json", HelpText = "Path to the configuration file for Prometheus File SD.")]
+        public string ConfigFilePath { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<AddOptions>(args).WithParsed(Add);
+        }
+
+        static void Add(AddOptions options)
         {
             using var provider = new ServiceCollection()
                 .AddLogging(conf => conf.AddConsole())
                 .BuildServiceProvider();
 
             var logger = provider.GetService<ILogger<Program>>();
-            logger.LogInformation("Configuring Prometheus file service discovery...");
+            logger.LogInformation("Adding target to Prometheus file service discovery...");
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Prometheus File Service Discovery Configuration Appender
 Command line utility to add and remove targets and jobs from Prometheus File Service Discovery configuration file.
+
+
+Link to prometheus documentation: https://prometheus.io/docs/guides/file-sd/
+Reference: https://github.com/amitsaha/prom-file-sd-config-generator
